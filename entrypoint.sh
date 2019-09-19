b84b6131705dcda3994a24d59ce12e58d3e2d050
Lucas Costi <lucascosti@github.com>
2019-09-19 13:34:14 +1000

    refinements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh -l
 
 name="$1"
-
 time=$(date)
+
 echo ::set-output name=time::$time
 
-if echo $name | grep -i "^homer" > /dev/null
-then
+if echo $name | grep -i "^homer" > /dev/null ; then
   echo "No Homers allowed!"
   # Exit with a failure
   exit 1
