fed9702df11434680afd80e29e4ca2bec16eee08
Lucas Costi <lucascosti@github.com>
2019-09-19 12:47:53 +1000

    Add exits on Homer hello

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+name="$1"
+
 time=$(date)
 echo ::set-output name=time::$time
+
+if echo $name | grep -i "^homer" > /dev/null
+then
+  echo "No Homers allowed!"
+  # Exit with a failure
+  exit 1
+else
+  echo "Hello $name"
+fi
+
