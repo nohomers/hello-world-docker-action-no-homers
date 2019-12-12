dfed1eebd42f9af09afd052bc7375eafccaa1424
Lucas Costi <lucascosti@users.noreply.github.com>
2019-12-12 13:13:41 +1000

    Add the number of homers allowed

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ name="$1"
 time=$(date)
 
 echo ::set-output name=time::$time
+::set-output name=homers-allowed::0
+
 
 if echo $name | grep -i "^homer" > /dev/null ; then
   echo "No Homers allowed!"
