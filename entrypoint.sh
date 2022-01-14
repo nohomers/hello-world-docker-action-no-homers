d36d610cccd6fbd67f2ac21a612a299dd3fba54a
Lucas Costi <lucascosti@users.noreply.github.com>
2022-01-14 11:00:43 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,12 @@ time=$(date)
 
 echo ::set-output name=time::$time
 
+echo "dumping context"
+
+echo GITHUB_ACTION_REPOSITORY $GITHUB_ACTION_REPOSITORY
+
+echo GITHUB_ACTION_REF $GITHUB_ACTION_REF
+
 if echo $name | grep -i "^homer" > /dev/null ; then
   echo "No Homers allowed!"
   # Exit with a failure
